a722694f1c9a3857f5180c52a8b73195f6285aff
Nic <nichostst@gmail.com>
2025-02-26 08:41:07 +0700

    Updated main script

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-from collections import Counter
-from stats import count_words
+from stats import count_words, bookstat
+
 
 def main():
-    with open('./books/frankenstein.txt') as o:
-        c = Counter()
-        content = o.read()
-        c.update(content.lower())
-        wordcount = count_words(content)
-        print("--- Begin report of books/frankenstein.txt ---")
-        print(f"{wordcount} words found in the document")
-        print("")
-        character: str = None
-        for character, times in c.items():
-            if character.isalpha():
-                print(f"The '{character}' character was found {times} times")
-        print("--- End report ---")
+    bookstat('./books/frankenstein.txt')
+    bookstat('./books/mobydick.txt')
+    bookstat('./books/prideandprejudice.txt')
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,20 @@
+from collections import Counter
+
+
 def count_words(content):
     return len(content.split())
+
+def bookstat(book):
+    with open(book) as o:
+        c = Counter()
+        content = o.read()
+        c.update(content.lower())
+        wordcount = count_words(content)
+        print("--- Begin report of books/frankenstein.txt ---")
+        print(f"{wordcount} words found in the document")
+        print("")
+        character: str = None
+        for character, times in c.items():
+            if character.isalpha():
+                print(f"{character}: {times}")
+        print("--- End report ---")
